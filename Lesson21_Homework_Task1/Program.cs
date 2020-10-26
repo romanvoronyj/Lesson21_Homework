397063fc270f88bb6156815224fa4463ea38861c
romanvoronyj <67970499+romanvoronyj@users.noreply.github.com>
2020-10-26 20:16:57 +0200

    Add files via upload

---
+++ Lesson21_Homework_Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lesson21_Homework_Task1
+{
+    /*Використовуючи Visual Studio 2010 створіть проект за шаблоном Console Application.
+      Напишіть програму, в якій метод буде викликатися рекурсивно. Кожен новий виклик методу виконується в окремому потоці.
+    */
+   class Program
+    {
+        static void Recursion(object num)
+        {
+            int n = (int)num;
+            if (n <= 0)
+                return;
+            Thread t = new Thread(Recursion);
+            t.Name = "Thread" + n;
+            t.Start(n - 1);
+            Console.WriteLine(t.Name);
+        }
+
+        static void Main(string[] args)
+        {
+            Recursion(10);
+
+            Console.ReadKey();
+        }
+    }
+}
